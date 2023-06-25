a977c8c4ab1300a387e08fde3d5028d3c2bee673
lukas <lukas.weber.4.12@gmail.com>
2023-06-25 17:15:43 +0200

    make games loop infinitely

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,37 @@
 use rand::Rng;
 
-const BLUE_COTTON_BALL: u32 = 0;
-const RED_COTTON_BALL: u32 = 1;
-const TOTAL_GAMES: u32 = 1000000000;
+const BLUE_COTTON_BALL: u128 = 0;
+const RED_COTTON_BALL: u128 = 1;
+const TOTAL_GAMES: u32 = 10000000;
 
 fn main() {
-    let mut total_rounds: u128 = 0;
+    loop {
+        let mut total_rounds: u128 = 0;
 
-    let mut random_generator = rand::thread_rng();
-    for _ in 0..TOTAL_GAMES {
-        let mut balls = 2;
-        let mut rounds_played = 0;
+        let mut random_generator = rand::thread_rng();
+        for _ in 0..TOTAL_GAMES {
+            let mut balls = 2;
 
-        loop {
-            let selected_ball = random_generator.gen_range(0..balls);
-            if selected_ball != BLUE_COTTON_BALL {
-                balls += RED_COTTON_BALL;
-                rounds_played += 1;
-                continue;
+            loop {
+                let selected_ball = random_generator.gen_range(0..balls);
+                if selected_ball != BLUE_COTTON_BALL {
+                    balls += RED_COTTON_BALL;
+                    continue;
+                }
+                break;
             }
-            break;
+
+            total_rounds += balls - 1;
         }
 
-        total_rounds += rounds_played;
+        println!("==== Results ============");
+        println!("{} games played", TOTAL_GAMES);
+        println!("{} rounds played", total_rounds);
+        println!(
+            "rounds per game: {}",
+            total_rounds as f64 / TOTAL_GAMES as f64
+        );
+        println!();
+        println!();
     }
-
-    println!("==== Results ============");
-    println!("{} games played", TOTAL_GAMES);
-    println!("{} rounds played", total_rounds);
-    println!("rounds per game: {}", total_rounds as f64 / TOTAL_GAMES as f64)
 }
