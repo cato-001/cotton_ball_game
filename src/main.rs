a1a9781794f45497482f016bd9e91da341d464a6
Lukas412 <lukas.weber.4.12@gmail.com>
2024-04-05 23:46:10 +0200

    add cli option to let user define the number of games to play

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,44 @@
+use clap::{value_parser, Arg, Command};
 use rand::Rng;
 
 const BLUE_COTTON_BALL: u128 = 0;
 const RED_COTTON_BALL: u128 = 1;
-const TOTAL_GAMES: u32 = 10000000;
 
 fn main() {
-    loop {
-        let mut total_rounds: u128 = 0;
+    let cli = Command::new("cotton-ball-game")
+        .args([Arg::new("count")
+            .long("count")
+            .short('c')
+            .value_parser(value_parser!(usize))
+            .help("how many games to run (default is 100000)")])
+        .about("run the cotton ball game")
+        .get_matches();
 
-        let mut random_generator = rand::thread_rng();
-        for _ in 0..TOTAL_GAMES {
-            let mut balls = 2;
+    let count: usize = cli.get_one("count").copied().unwrap_or(100000);
 
-            loop {
-                let selected_ball = random_generator.gen_range(0..balls);
-                if selected_ball != BLUE_COTTON_BALL {
-                    balls += RED_COTTON_BALL;
-                    continue;
-                }
-                break;
+    let mut total_rounds: u128 = 0;
+
+    let mut random_generator = rand::thread_rng();
+
+    for _ in 0..count {
+        let mut balls = 2;
+
+        loop {
+            let selected_ball = random_generator.gen_range(0..balls);
+            if selected_ball != BLUE_COTTON_BALL {
+                balls += RED_COTTON_BALL;
+                continue;
             }
-
-            total_rounds += balls - 1;
+            break;
         }
 
-        println!("==== Results ============");
-        println!("{} games played", TOTAL_GAMES);
-        println!("{} rounds played", total_rounds);
-        println!(
-            "rounds per game: {}",
-            total_rounds as f64 / TOTAL_GAMES as f64
-        );
-        println!();
-        println!();
+        total_rounds += balls - 1;
     }
+
+    println!("==== Results ============");
+    println!("{} games played", count);
+    println!("{} rounds played", total_rounds);
+    println!("rounds per game: {}", total_rounds as f64 / count as f64);
+    println!();
+    println!();
 }
